1b35d94615991d68f47e1c49d5506c92fa280048
Vitória Comiran <vicomiran@gmail.com>
2025-07-10 23:22:29 -0300

    atualização dia 4 - tipo de lista

---
+++ Listas/tipos_listas.py
@@ -0,0 +1,15 @@
+# lista preenchida estaticamente
+lista_estatica = ["xpto", True]
+
+# lista preenchida dinamicamente
+lista_dinamica = [input("Digite um usuário: "), bool(int(input("Está logado? ")))]
+
+# lista vazia
+lista_vazia = []
+
+# obs1: em todos os casos os valores devem estar entre [], mesmo a lista sendo vazia.
+
+# obs2: na lista dinamica o segundo item passa por duas conversões:
+# a primeira para transformar a string (valor natural do input/sempre retorna string) em um int
+# a segunda para transformar o int em um boolean (True / False)
+
