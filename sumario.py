c230ac9d11a5a0985843388538ce698b77fdfebe
Vitória Comiran <vicomiran@gmail.com>
2025-07-15 21:59:07 -0300

    Sumário dos estudos

---
+++ sumario.py
@@ -0,0 +1,24 @@
+##Python:
+## => usado para automação de processos
+## => desenvolvimento de testes para endereçamento lógicos
+## => transmissão atuomática de informações entre dispositicos IOT
+
+##É multiparadima => suporta vários estilos diferentes de códigos
+##Linguagem de alto nível
+
+##Utiliza variáveis para armazenar os dados dentro da memória RAM; as variáveis são idenficadores, nomes para catalogar
+
+##Dados armazenados nas variáveis são do tipo numérico (int), (float), ou alfa numéricos (string), (boolean).for
+
+
+##Tomadas de decisão => responsável por fazer o código executar comandos
+    ##Podem ser simples, compostas ou encadeadas.
+
+##Laços de repetição => automatizam a repetição de linhas de códigos através de while e for.
+
+##Listas => formam uma estrutura para armazenar grandes quantidades de dados de maneira organizada.
+
+    ##Lista estática, dinâmica, vazia
+
+##Funções => responsável por automatizar ações específicas de parte do código.
+
